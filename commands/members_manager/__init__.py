5dff2861d762e0bf6acf2a11adbf55dece543837
Yury Yushmanov <46781434+LordRalInc@users.noreply.github.com>
2020-08-31 17:03:46 +0300

    2.0... Начало

--- commands/members_manager/__init__.py
+++
@@ -1,9 +0,0 @@
-from commands.members_manager import ignored
-from commands.members_manager import ignored_global
-from commands.members_manager import muted
-
-users_bp = (
-    ignored.user,
-    ignored_global.user,
-    muted.user,
-)
--- error_handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from error_handlers import captha
-from error_handlers import rps
-
-error_handlers_bp = (
-    rps.user,
-    captha.user,
-)
